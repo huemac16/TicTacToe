300996c34dda1ce34d2bce0be533b0c7f9e3ae66
huemac16 <huemac16@htl-kaindorf.at>
2018-11-30 12:28:15 +0100

    extended JFrame

--- TicTacToe/src/GUI/TicTacToeGUI.java
+++ TicTacToe/src/GUI/TicTacToeGUI.java
@@ -1,14 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package GUI;
 
-/**
- *
- * @author marce
- */
-public class TicTacToeGUI {
-    
+import java.awt.GridLayout;
+import java.awt.HeadlessException;
+import javax.swing.JFrame;
+
+public class TicTacToeGUI extends JFrame {
+
+    public TicTacToeGUI() throws HeadlessException {
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(500, 500);
+        this.setLayout(new GridLayout(3, 3));
+        
+    }
+
+    public static void main(String[] args) {
+        new TicTacToeGUI().setVisible(true);
+    }
+
 }
