5b0a58da42379c5d49c3b85f866e324143563cfd
huemac16 <huemac16@htl-kaindorf.at>
2018-11-30 12:37:42 +0100

    working a bit

--- TicTacToe/src/GUI/TicTacToeGUI.java
+++ TicTacToe/src/GUI/TicTacToeGUI.java
@@ -1,16 +1,35 @@
 package GUI;
 
+import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class TicTacToeGUI extends JFrame {
 
     public TicTacToeGUI() throws HeadlessException {
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setTitle("TicTacToe");
         this.setSize(500, 500);
         this.setLayout(new GridLayout(3, 3));
-        
+
+        for (int i = 0; i <= 8; i++) {
+            JButton button = new JButton();
+            button.setOpaque(true);
+            button.setBackground(Color.BLACK);
+            this.add(button);
+            button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent ae) {
+                    button.setBackground(Color.RED);
+                }
+            });
+
+        }
+
     }
 
     public static void main(String[] args) {
